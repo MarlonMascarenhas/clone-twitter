654f99033ec5347f8e8a2bcbc54bcb04ea94ec3e
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-16 19:25:46 -0300

    estilizacao container main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Layout />
       <GlobalStyles />
     </> 
   );
