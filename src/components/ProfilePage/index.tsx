211be2c5a83c188f98cd2a9eae282a88a5f5ee6d
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-17 16:01:10 -0300

    esqueleto ProfilePage

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Banner, Avatar, ProfileData, LocationIcon, CakeIcon, Followage } from './styles';
 
 const ProfilePage: React.FC = () => {
-  return <div />;
+    return (
+      <Container>
+            <Banner>
+                <Avatar />
+            </Banner>
+            <ProfileData>
+                {/* <EditButton outlined> Editar perfil</EditButton> */}
+                <h1>Marlon Mascarenhas</h1>
+                <h2>@MarlonMascarenhas</h2>
+                
+                <p>
+                    Developer <a href="https://www.linkedin.com/in/marlonmascarenhas/"> Linkedin </a> 
+                </p>
+
+                <ul>
+                    <li>
+                        <LocationIcon />
+                        Minas Gerais 
+                    </li>
+                    <li>
+                        <CakeIcon />
+                        Nascido(a) em 25 de marco de 1998 
+                    </li>
+                </ul>
+
+                <Followage>
+                    <span>
+                        seguindo <strong>719</strong>
+                    </span>
+                    <span>
+                        <strong>999 </strong> seguidores
+                    </span>
+
+                </Followage>
+            </ProfileData>
+
+        </Container>
+    );
 }
 
 export default ProfilePage;
