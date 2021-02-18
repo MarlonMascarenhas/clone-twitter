3a3bfb71c6673a7b68a7d69081e3522375d838f4
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-18 13:50:26 -0300

    realocacao do Rocketseaticon para components

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -7,7 +7,7 @@ export {
     LocationOn,
     Search,
 } from 'styled-icons/material-outlined';
-export { RocketseatIcon as Rocketseat } from '../Rocketseaticon';
+export { RocketseatIcon as Rocketseat } from '../components/RocketseatIcon';
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
 export { Chat } from 'styled-icons/heroicons-outline';
