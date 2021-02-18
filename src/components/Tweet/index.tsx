9df3ad8314b35809a0650b2a1efc78afc4853d38
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-18 13:31:30 -0300

    esqueleto Tweet

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Retweeted, RocketseatIcon, Body, Avatar, Content, Header, Description, ImageContent, Icons, Status, CommentIcon, RetweetIcon, LikeIcon  } from './styles';
 
 const Tweet: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        voce retweetou
+      </Retweeted>
+      <Body>
+        <Avatar />
+
+        <Content>
+            <Header>
+              <strong>Vacao</strong>
+              <span>@vacao</span>
+              <Dot />
+              <time>27 de jan</time>
+            </Header>
+            <Description>Foguete nao tem re</Description>
+
+            <ImageContent />
+            
+            <Icons>
+              <Status>
+                <CommentIcon />
+                199
+              </Status>
+              <Status>
+                <RetweetIcon />
+                199
+              </Status>
+              <Status>
+                <LikeIcon />
+                199
+              </Status>
+            </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
 }
 
 export default Tweet;
