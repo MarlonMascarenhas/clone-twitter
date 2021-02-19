fc8b961421b6ad56f22aaf8c25169784fff52e77
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-19 16:10:37 -0300

    News estilizada, comportamento de scroll do twitter

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
 import { Container, SearchWrapper, SearchInput, SearchIcon, Body} from './styles';
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
 
 const SideBar: React.FC = () => {
     return (
@@ -11,33 +13,76 @@ const SideBar: React.FC = () => {
                 <SearchInput placeholder = 'Buscar no Twitter' />
                 <SearchIcon />
             </SearchWrapper>
-            <Body>
-                <List
-                    title="Talvez voce curta"
-                    elements={[
-                        <FollowSuggestion 
-                            name="teste"
-                            nickname="@testetestado"
-                        />,
-                        <FollowSuggestion 
-                            name="teste"
-                            nickname="@testetestado"
-                        />,
-                        <FollowSuggestion 
-                            name="teste"
-                            nickname="@testetestado"
-                        />
-                    ]}
-                />
-                <List
-                    title="Talvez voce curta"
-                    elements={[
-                        <h1>Test</h1>,
-                        <h1>Test</h1>,
-                        <h1>Test</h1>
-                    ]}
-                />
-            </Body>
+            <StickyBox>
+                <Body>
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <FollowSuggestion 
+                                name="teste"
+                                nickname="@testetestado"
+                            />,
+                            <FollowSuggestion 
+                                name="teste"
+                                nickname="@testetestado"
+                            />,
+                            <FollowSuggestion 
+                                name="teste"
+                                nickname="@testetestado"
+                            />
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    <List
+                        title="Talvez voce curta"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>
+                        ]}
+                    />
+                    
+                </Body>
+            </StickyBox>
         </Container>
     );
 }
